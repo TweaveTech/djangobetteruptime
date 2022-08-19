d967a23741ebccd66c737b41efc6378ae3d68da0
Sascha Dobbelaere <sascha@tweave.tech>
2022-08-19 12:15:05 +0100

    fix self reference -and bump version to 0.4

--- betteruptime/decorators.py
+++ betteruptime/decorators.py
@@ -10,7 +10,7 @@ def send_heartbeat(heartbeat_id=None, run_in_debug=False):
     '''
     def dec_f(f):
         def wrap_f(self, *args, **kwargs):
-            f_res = f(*args, **kwargs)
+            f_res = f(self, *args, **kwargs)
 
             heartbeat_id = heartbeat_id or self.heartbeat_id
             sound_alive(heartbeat_id)
