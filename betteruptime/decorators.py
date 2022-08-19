4b5b5276bcbe709f19184bdf19a9873bd1628a27
Sascha Dobbelaere <sascha@tweave.tech>
2022-08-19 13:27:51 +0100

    stab at fixing decorator

--- betteruptime/decorators.py
+++ betteruptime/decorators.py
@@ -1,3 +1,4 @@
+from functools import wraps
 import requests
 from .helpers import sound_alive
 
@@ -9,6 +10,7 @@ def send_heartbeat(heartbeat_id=None, run_in_debug=False):
     When better-uptime doesnt receive it's heartbeats in time, some action will be triggered.
     '''
     def dec_f(f):
+        @wraps(f)
         def wrap_f(self, *args, **kwargs):
             f_res = f(self, *args, **kwargs)
 
