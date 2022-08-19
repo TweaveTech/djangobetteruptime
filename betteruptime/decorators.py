cc157738163e104581a98327dec054978ffaefda
Sascha Dobbelaere <sascha@tweave.tech>
2022-08-19 13:45:16 +0100

    fiddling to get the decorator to work :/

--- betteruptime/decorators.py
+++ betteruptime/decorators.py
@@ -1,6 +1,31 @@
-from functools import wraps
-import requests
+# from functools import wraps
+# import requests
 from .helpers import sound_alive
+
+
+# def send_heartbeat(heartbeat_id=None, run_in_debug=False):
+#     '''
+#     Send a heartbeat to the given better-uptime url if the function ran successfully.
+#     If however the function failed, it should send a heartbeat at all.
+#     When better-uptime doesnt receive it's heartbeats in time, some action will be triggered.
+#     '''
+#     def dec_f(f):
+#         @wraps(f)
+#         def wrap_f(self, *args, **kwargs):
+#             f_res = f(self, *args, **kwargs)
+
+#             if not heartbeat_id:
+#                 heartbeat_id = self.heartbeat_id
+                
+#             sound_alive(heartbeat_id)
+
+#             return f_res
+#         return wrap_f
+#     return dec_f
+
+
+from django.apps import apps
+import requests
 
 
 def send_heartbeat(heartbeat_id=None, run_in_debug=False):
@@ -10,16 +35,15 @@ def send_heartbeat(heartbeat_id=None, run_in_debug=False):
     When better-uptime doesnt receive it's heartbeats in time, some action will be triggered.
     '''
     def dec_f(f):
-        @wraps(f)
-        def wrap_f(self, *args, **kwargs):
-            f_res = f(self, *args, **kwargs)
+        def wrap_f(*args, **kwargs):
+            f_res = f(*args, **kwargs)
 
-            if not heartbeat_id:
-                heartbeat_id = self.heartbeat_id
-                
             sound_alive(heartbeat_id)
-
+            # heartbeat_host = apps.get_app_config('betteruptime').heartbeat_host
+            # heartbeat_url = heartbeat_host + heartbeat_id
+            # requests.get(heartbeat_url)
             return f_res
         return wrap_f
     return dec_f
+
 
--- betteruptime/helpers.py
+++ betteruptime/helpers.py
@@ -5,6 +5,7 @@ import requests
 def create_heartbeat_url(heartbeat_id):
 	heartbeat_host = apps.get_app_config('betteruptime').heartbeat_host
 	heartbeat_url = heartbeat_host + heartbeat_id
+	return heartbeat_url
 
 
 def sound_alive(heartbeat_id):
