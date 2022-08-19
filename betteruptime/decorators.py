3e1b2d524e9bec30b199e9fa0aa2ffba949d0926
Sascha Dobbelaere <sascha@tweave.tech>
2022-08-19 13:50:21 +0100

    Improving deco

--- betteruptime/decorators.py
+++ betteruptime/decorators.py
@@ -1,30 +1,6 @@
-# from functools import wraps
-# import requests
-from .helpers import sound_alive
-
-
-# def send_heartbeat(heartbeat_id=None, run_in_debug=False):
-#     '''
-#     Send a heartbeat to the given better-uptime url if the function ran successfully.
-#     If however the function failed, it should send a heartbeat at all.
-#     When better-uptime doesnt receive it's heartbeats in time, some action will be triggered.
-#     '''
-#     def dec_f(f):
-#         @wraps(f)
-#         def wrap_f(self, *args, **kwargs):
-#             f_res = f(self, *args, **kwargs)
-
-#             if not heartbeat_id:
-#                 heartbeat_id = self.heartbeat_id
-                
-#             sound_alive(heartbeat_id)
-
-#             return f_res
-#         return wrap_f
-#     return dec_f
-
-
 from django.apps import apps
+from .helpers import sound_alive
+from .exceptions import NoHeartBeatIdError
 import requests
 
 
@@ -38,10 +14,13 @@ def send_heartbeat(heartbeat_id=None, run_in_debug=False):
         def wrap_f(*args, **kwargs):
             f_res = f(*args, **kwargs)
 
-            sound_alive(heartbeat_id)
-            # heartbeat_host = apps.get_app_config('betteruptime').heartbeat_host
-            # heartbeat_url = heartbeat_host + heartbeat_id
-            # requests.get(heartbeat_url)
+            if heartbeat_id:
+                sound_alive(heartbeat_id)
+            else:
+                try:
+                    sound_alive(self.heartbeat_id)
+                except AttributeError:
+                    raise NoHeartBeatIdError()
             return f_res
         return wrap_f
     return dec_f
---
+++ betteruptime/exceptions.py
@@ -0,0 +1,2 @@
+class NoHeartBeatIdError(Exception):
+    pass
