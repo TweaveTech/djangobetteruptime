9e81ba4a3b252b43494c23486e9e7fba12d54a45
BranDavidSebastian <brandavid1998@gmail.com>
2023-07-14 22:47:03 +0100

    #379 change hostname ac v06

--- betteruptime/apps.py
+++ betteruptime/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class BetteruptimeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'betteruptime'
-    heartbeat_host = "https://betteruptime.com/api/v1/heartbeat/"
+    heartbeat_host = "https://uptime.betterstack.com/api/v1/heartbeat/"
